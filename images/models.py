9b67d8e31f23d88cf92d037caa50382fc2e9f59f
Niko  Lillios <niko@brilliant.tech>
2025-04-29 21:18:26 -0700

    Add ditheredimagemodel

--- images/models.py
+++ images/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 import account.models as acc_models
-from .services.image_service import userImagesDirPath
+from .services.image_service import userImagesDirPath, ditheredImagesDirPath
 
 class ImageModel(models.Model):
     owner = models.ForeignKey(acc_models.UserData, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=userImagesDirPath, null=True)
 
+class DitheredImageModel(models.Model):
+    owner = models.ForeignKey(acc_models.UserData, on_delete=models.CASCADE)
+    original_image = models.ForeignKey(ImageModel, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=ditheredImagesDirPath, null=True)
--- picturesque/admin.py
+++ picturesque/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from images.models import ImageModel
+from images.models import ImageModel, DitheredImageModel
 
 admin.site.register(ImageModel)
+admin.site.register(DitheredImageModel)
