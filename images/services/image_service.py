adcf12796181a2cc3893f25bce95091b2455ae39
Niko  Lillios <niko@brilliant.tech>
2025-04-29 21:19:10 -0700

    save dithered image set size

--- images/services/image_service.py
+++ images/services/image_service.py
@@ -10,7 +10,10 @@ def getImagesByUserID(user):
     return [image.id for image in images]
 
 def userImagesDirPath(instance, filename):
-    return 'user_{0}/{1}'.format(instance.owner.id, filename)
+    return 'user_{0}/original/{1}'.format(instance.owner.id, filename)
+
+def ditheredImagesDirPath(instance, filename):
+    return 'user_{0}/dithered/{1}'.format(instance.owner.id, filename)
 
 def findClosestE6PaletteColor(pixel):
     colors = np.array([[1, 1, 1], [0, 0, 0], [1, 1, 0], [1, 0, 0], [0, 0, 1], [0, 0, 1]])
@@ -24,8 +27,8 @@ def findClosestE6PaletteColor(pixel):
     return closestColor
 
 def ditherFloydSteinberg(image):
-    shape = image.shape
     image = np.divide(image, 255)
+    shape = image.shape
     for i in range(shape[0]):
         for j in range(shape[1]):
             # newPixel = colors[np.argmin(
@@ -43,11 +46,11 @@ def ditherFloydSteinberg(image):
                 image[i+1, j] = image[i+1, j] + quantErr * 5/16
                 if j > 0:
                     image[i+1, j-1] = image[i+1, j-1] + quantErr * 3/16
-    return image
+    return Image.fromarray(np.array(image, dtype=np.uint8))
 
 def ditherAtkinson(image):
-    shape = image.shape
     image = np.divide(image, 255)
+    shape = image.shape
     for i in range(shape[0]):
         for j in range(shape[1]):
             newPixel = findClosestE6PaletteColor(image[i, j, :])
@@ -65,7 +68,7 @@ def ditherAtkinson(image):
                     image[i+1, j-1] = image[i+1, j-1] + quantErr * 1/8
                 if i < shape[0] - 2:
                     image[i+2, j] = image[i+2, j] + quantErr * 1/8
-    return image
+    return Image.fromarray(np.array(image, dtype=np.uint8))
 
 
 def testDitherFloydSteinberg():
--- images/views.py
+++ images/views.py
@@ -1,15 +1,17 @@
 from django.core.files import File
 from django.core.files.images import ImageFile
+from django.core.files.base import ContentFile
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from .models import ImageModel
+from .models import ImageModel, DitheredImageModel
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .services.image_service import getImagesByUserID
+from .services.image_service import getImagesByUserID, ditherAtkinson
 from account.models import UserData
 import io, base64
+import numpy as np
 import uuid
 from PIL import Image
 from .serializers import ImageSerializer
@@ -31,14 +33,23 @@ def getUserImages(request):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def uploadImageFile(request):
-    print(f"Saving image! User: {request.user}")
-    # print(f"data: {request.data['image'][:100]}")
     base64Str = request.data["image"].split(',')[1]
     imageBytes = io.BytesIO(base64.decodebytes(bytes(base64Str, "utf-8")))
     imgFile = ImageFile(imageBytes, f'{uuid.uuid4()}.png')
+
     imgModel = ImageModel.objects.create(
         owner=request.user,
         image=imgFile,
     )
-    print("recevied image")
+
+    image = Image.open(imgFile).convert('RGB')
+    dithered_image = ditherAtkinson(image)
+    dithered_io = io.BytesIO()
+    dithered_image.save(dithered_io, format="JPEG")
+    dithered_file = ContentFile(dithered_io.getvalue())
+    dith_model = DitheredImageModel.objects.create(
+        owner=request.user,
+        original_image=imgModel,
+        image=ImageFile(dithered_file, f'{uuid.uuid4()}.jpg')
+    )
     return HttpResponse("Successfully received image")
