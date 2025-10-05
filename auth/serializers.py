bc5e539835a60166e0e64e50afe535b9fa5e3ae5
Niko  Lillios <niko@brilliant.tech>
2025-10-04 20:21:53 -0700

    Add RasberryPi model serializer

--- auth/serializers.py
+++ auth/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import RaspberryPi
+from rest_framework import serializers
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -6,3 +8,10 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
         token['uid'] = user.id
         return token
+
+
+class RaspberryPiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RaspberryPi
+        fields = ['serial_id', 'is_active', 'is_paired', 'created_at', 'last_seen']
+        read_only_fields = ['serial_id', 'created_at', 'last_seen']
